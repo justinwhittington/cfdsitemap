519cc21448f9510fdbe680c89496331368f46533
Justin <jwhittington1992@gmail.com>
2019-05-01 06:20:28 -0400

    2

--- siteMap.js
+++ siteMap.js
@@ -52,6 +52,8 @@ traverse = () => {
 
   levelOne = sitemap[0].previousElementSibling.childNodes;
 
+  
+
   for (let a = 0; a < levelOne.length; a++) {
     if (levelOne[a].innerText) {
       let lvl1 = levelOne[a].innerText;
@@ -63,126 +65,126 @@ traverse = () => {
       /*
       This next group of loops finds the second level of indexing
     */
-      for (let b = 0; b < sitemap[0].children.length; b++) {
-        two = 0;
-        for (let c = 0; c < sitemap[0].children[b].childNodes.length; c++) {
-          if (!sitemap[0].children[b].childNodes[c].children) {
-            continue;
-          } else {
-            for (
-              let d = 0;
-              d < sitemap[0].children[b].childNodes[c].children.length;
-              d++
+   if (one == 2) {
+    for (let b = 0; b < sitemap[0].children.length; b++) {
+      two = 0;
+      for (let c = 0; c < sitemap[0].children[b].childNodes.length; c++) {
+        if (!sitemap[0].children[b].childNodes[c].children) {
+          continue;
+        } else {
+          for (
+            let d = 0;
+            d < sitemap[0].children[b].childNodes[c].children.length;
+            d++
+          ) {
+            if (
+              !sitemap[0].children[b].childNodes[c].children[d].childNodes
             ) {
-              if (
-                !sitemap[0].children[b].childNodes[c].children[d].childNodes
+              continue;
+            } else {
+              for (
+                let e = 0;
+                e <
+                sitemap[0].children[b].childNodes[c].children[d].childNodes
+                  .length;
+                e++
               ) {
-                continue;
-              } else {
-                for (
-                  let e = 0;
-                  e <
-                  sitemap[0].children[b].childNodes[c].children[d].childNodes
-                    .length;
-                  e++
+                if (
+                  !sitemap[0].children[b].childNodes[c].children[d]
+                    .childNodes[e].localName
                 ) {
-                  if (
-                    !sitemap[0].children[b].childNodes[c].children[d]
-                      .childNodes[e].localName
+                  continue;
+                } else if (
+                  sitemap[0].children[b].childNodes[c].children[d].childNodes[
+                    e
+                  ].localName == "a"
+                ) {
+                  let lvl2 =
+                    sitemap[0].children[b].childNodes[c].children[d]
+                      .childNodes[e].innerText;
+                  two++;
+                  console.log(
+                    `    ${one}.${two}.${three}.${four}.${five}.${six}.${seven}.${eight}.${nine} ${lvl2}`
+                  );
+                } else if (
+                  sitemap[0].children[b].childNodes[c].children[d].childNodes[
+                    e
+                  ].localName == "ol"
+                ) {
+                  /* Now we get the third */
+                  three = 0;
+                  for (
+                    let f = 0;
+                    f <
+                    sitemap[0].children[b].childNodes[c].children[d]
+                      .childNodes[e].childNodes.length;
+                    f++
                   ) {
-                    continue;
-                  } else if (
-                    sitemap[0].children[b].childNodes[c].children[d].childNodes[
-                      e
-                    ].localName == "a"
-                  ) {
-                    let lvl2 =
+                    if (
                       sitemap[0].children[b].childNodes[c].children[d]
-                        .childNodes[e].innerText;
-                    two++;
-                    console.log(
-                      `    ${one}.${two}.${three}.${four}.${five}.${six}.${seven}.${eight}.${nine} ${lvl2}`
-                    );
-                  } else if (
-                    sitemap[0].children[b].childNodes[c].children[d].childNodes[
-                      e
-                    ].localName == "ol"
-                  ) {
-                    /* Now we get the third */
-                    three = 0;
-                    for (
-                      let f = 0;
-                      f <
-                      sitemap[0].children[b].childNodes[c].children[d]
-                        .childNodes[e].childNodes.length;
-                      f++
+                        .childNodes[e].childNodes[f].localName
                     ) {
-                      if (
+                      let lvl3 =
                         sitemap[0].children[b].childNodes[c].children[d]
-                          .childNodes[e].childNodes[f].localName
+                          .childNodes[e].childNodes[f].childNodes[1]
+                          .innerText;
+                      three++;
+                      console.log(
+                        `        ${one}.${two}.${three}.${four}.${five}.${six}.${seven}.${eight}.${nine} ${lvl3}`
+                      );
+                      /* Now the fourth*/
+                      for (
+                        let g = 0;
+                        g <
+                        sitemap[0].children[b].childNodes[c].children[d]
+                          .childNodes[e].childNodes[f].childNodes.length;
+                        g++
                       ) {
-                        let lvl3 =
+                        four = 0;
+                        if (
                           sitemap[0].children[b].childNodes[c].children[d]
-                            .childNodes[e].childNodes[f].childNodes[1]
-                            .innerText;
-                        three++;
-                        console.log(
-                          `        ${one}.${two}.${three}.${four}.${five}.${six}.${seven}.${eight}.${nine} ${lvl3}`
-                        );
-                        /* Now the fourth*/
-                        for (
-                          let g = 0;
-                          g <
-                          sitemap[0].children[b].childNodes[c].children[d]
-                            .childNodes[e].childNodes[f].childNodes.length;
-                          g++
+                            .childNodes[e].childNodes[f].childNodes[g]
+                            .localName == "ol"
                         ) {
-                          four = 0;
-                          if (
+                          for (
+                            let h = 0;
+                            h <
                             sitemap[0].children[b].childNodes[c].children[d]
                               .childNodes[e].childNodes[f].childNodes[g]
-                              .localName == "ol"
+                              .childNodes.length;
+                            h++
                           ) {
-                            for (
-                              let h = 0;
-                              h <
+                            if (
                               sitemap[0].children[b].childNodes[c].children[d]
                                 .childNodes[e].childNodes[f].childNodes[g]
-                                .childNodes.length;
-                              h++
+                                .childNodes[h].localName == "li"
                             ) {
-                              if (
-                                sitemap[0].children[b].childNodes[c].children[d]
-                                  .childNodes[e].childNodes[f].childNodes[g]
-                                  .childNodes[h].localName == "li"
-                              ) {
-                                four++;
-                                console.log(
-                                  `            ${one}.${two}.${three}.${four}.${five}.${six}.${seven}.${eight}.${nine} ${
-                                    sitemap[0].children[b].childNodes[c]
-                                      .children[d].childNodes[e].childNodes[f]
-                                      .childNodes[g].childNodes[h].childNodes[1]
-                                      .innerText
-                                  }`
-                                );
-                              }
-                              /* Now the fifth */
-                              five = 0;
-                              for (let i = 0; i <  sitemap[0].children[b].childNodes[c]
-                                .children[d].childNodes[e].childNodes[f]
-                                .childNodes[g].childNodes[h].childNodes.length; i++) {
-                                  if (sitemap[0].children[b].childNodes[c]
+                              four++;
+                              console.log(
+                                `            ${one}.${two}.${three}.${four}.${five}.${six}.${seven}.${eight}.${nine} ${
+                                  sitemap[0].children[b].childNodes[c]
                                     .children[d].childNodes[e].childNodes[f]
-                                    .childNodes[g].childNodes[h].childNodes[i].localName == "ol") {
-                                          five++;
-                                          console.log(`                ${one}.${two}.${three}.${four}.${five}.${six}.${seven}.${eight} ${sitemap[0].children[b].childNodes[c]
-                                            .children[d].childNodes[e].childNodes[f]
-                                            .childNodes[g].childNodes[h].childNodes[i].childNodes[1].innerText}`)
-                                    }
-                                }
+                                    .childNodes[g].childNodes[h].childNodes[1]
+                                    .innerText
+                                }`
+                              );
                             }
-
+                            /* Now the fifth */
+                            five = 0;
+                            for (let i = 0; i <  sitemap[0].children[b].childNodes[c]
+                              .children[d].childNodes[e].childNodes[f]
+                              .childNodes[g].childNodes[h].childNodes.length; i++) {
+                                if (sitemap[0].children[b].childNodes[c]
+                                  .children[d].childNodes[e].childNodes[f]
+                                  .childNodes[g].childNodes[h].childNodes[i].localName == "ol") {
+                                        five++;
+                                        console.log(`                ${one}.${two}.${three}.${four}.${five}.${six}.${seven}.${eight} ${sitemap[0].children[b].childNodes[c]
+                                          .children[d].childNodes[e].childNodes[f]
+                                          .childNodes[g].childNodes[h].childNodes[i].childNodes[1].innerText}`)
+                                  }
+                              }
                           }
+
                         }
                       }
                     }
@@ -193,6 +195,9 @@ traverse = () => {
           }
         }
       }
+    }
+   }
+      
     }
   }
 };
