f96edc636dbfcadc617c57c5c5af84737ffdc769
Justin <jwhittington1992@gmail.com>
2019-05-11 15:22:28 -0400

    indexd sitemap

--- siteMap.js
+++ siteMap.js
@@ -2,189 +2,157 @@ let sitemap = document.querySelectorAll(".siteMap");
 
 console.log(sitemap);
 
-// function traverse2() {
-//   let one = 0,
-//     two = 0,
-//     three = 0,
-//     four = 0,
-//     five = 0,
-//     six = 0,
-//     seven = 0,
-//     eight = 0,
-//     nine = 0;
-  
-
-  // Loop for ancestor == Home Government etc
-//   for (let a = 0; a < sitemap[0].children.length; a++) {
-//     one++;
-//     // Some of the elements have the entire list inside their innerText, and we need a way to stop the loop and go inside the nested list
-//     if (sitemap[0].children[a].innerText.length < 30) { 
-//       console.log(
-//         `${one}.${two}.${three}.${four}.${five}.${six}.${seven}.${eight}.${nine} ${
-//           sitemap[0].children[a].innerText
-//         }`
-//       );
-//     } else if (sitemap[0].children[a].innerText.length > 30) {
-//       let lvl1 = sitemap[0].children[a];
-//       console.log(
-//         `${one}.${two}.${three}.${four}.${five}.${six}.${seven}.${eight}.${nine} ${
-//           lvl1.childNodes[1].innerText
-//         }`
-//       );
-//       // Now we go through the first nested list
-//       for (let b = 0; b < lvl1.children[1].children.length; b++) {
-//         console.log(lvl1.children[1].children[a].childNodes[1].innerText)
-//       }
-//     }
-//   }
-// }
-
-// traverse2();
-
-traverse = () => {
+function mapSite() {
   let one = 0,
     two = 0,
     three = 0,
     four = 0,
     five = 0,
     six = 0,
-    seven = 0, eight = 0, nine = 0;
-
-  levelOne = sitemap[0].previousElementSibling.childNodes;
-
-  
-
-  for (let a = 0; a < levelOne.length; a++) {
-    if (levelOne[a].innerText) {
-      let lvl1 = levelOne[a].innerText;
+    seven = 0,
+    eight = 0,
+    nine = 0;
+  // The first 'layer' of the sitemap list. Each child is a 'li' containing deeper nesting
+  let layer1 = sitemap[0].children;
+  // loop through layer 1
+  for (let a = 0; a < layer1.length; a++) {
+    two = 0;
+    // layer1List is the list of outer titles. Each child from layer1 has children with the list title
+    let layer1List = layer1[a].children[0];
+    if (layer1List.innerText) {
       one++;
       console.log(
-        `${one}.${two}.${three}.${four}.${five}.${six}.${seven}.${eight}.${nine} ${lvl1}`
+        `${one}.${two}.${three}.${four}.${five}.${six}.${seven}.${eight}.${nine} ${
+          layer1List.innerText
+        }`
       );
-
-      /*
-      This next group of loops finds the second level of indexing
-    */
-   if (one >= 2) {
-    for (let b = 0; b < sitemap[0].children.length; b++) {
+    }
+    // Passes 'Home'
+    if (one > 1) {
       two = 0;
-      for (let c = 0; c < sitemap[0].children[b].childNodes.length; c++) {
-        if (!sitemap[0].children[b].childNodes[c].children) {
-          continue;
-        } else {
-          for (
-            let d = 0;
-            d < sitemap[0].children[b].childNodes[c].children.length;
-            d++
+      if (layer1[a].childNodes[3].localName == "ol") {
+        // The Second 'layer of the sitemap list
+        let layer2 = layer1[a].childNodes[3].children;
+        // loop through layer two and log the pages
+        for (let b = 0; b < layer2.length; b++) {
+          three = 0;
+          two++;
+          console.log(
+            `${one}.${two}.${three}.${four}.${five}.${six}.${seven}.${eight}.${nine} ${
+              layer2[b].children[0].innerText
+            }`
+          );
+
+          if (
+            layer2[b].childNodes[3] &&
+            layer2[b].childNodes[3].localName == "ol"
           ) {
-            if (
-              !sitemap[0].children[b].childNodes[c].children[d].childNodes
-            ) {
-              continue;
-            } else {
-              for (
-                let e = 0;
-                e <
-                sitemap[0].children[b].childNodes[c].children[d].childNodes
-                  .length;
-                e++
+            let layer3 = layer2[b].childNodes[3].children;
+            // Loops third layer
+            for (let c = 0; c < layer3.length; c++) {
+              four = 0;
+              three++;
+              console.log(
+                `  ${one}.${two}.${three}.${four}.${five}.${six}.${seven}.${eight}.${nine} ${
+                  layer3[c].children[0].innerText
+                }`
+              );
+
+              if (
+                layer3[c].childNodes[3] &&
+                layer3[c].childNodes[3].localName == "ol"
               ) {
-                if (
-                  !sitemap[0].children[b].childNodes[c].children[d]
-                    .childNodes[e].localName
-                ) {
-                  continue;
-                } else if (
-                  sitemap[0].children[b].childNodes[c].children[d].childNodes[
-                    e
-                  ].localName == "a"
-                ) {
-                  let lvl2 =
-                    sitemap[0].children[b].childNodes[c].children[d]
-                      .childNodes[e].innerText;
-                  two++;
+                let layer4 = layer3[c].childNodes[3].children;
+
+                for (let d = 0; d < layer4.length; d++) {
+                  five = 0;
+                  four++;
                   console.log(
-                    `    ${one}.${two}.${three}.${four}.${five}.${six}.${seven}.${eight}.${nine} ${lvl2}`
+                    `    ${one}.${two}.${three}.${four}.${five}.${six}.${seven}.${eight}.${nine} ${
+                      layer4[d].children[0].innerText
+                    }`
                   );
-                } else if (
-                  sitemap[0].children[b].childNodes[c].children[d].childNodes[
-                    e
-                  ].localName == "ol"
-                ) {
-                  /* Now we get the third */
-                  three = 0;
-                  for (
-                    let f = 0;
-                    f <
-                    sitemap[0].children[b].childNodes[c].children[d]
-                      .childNodes[e].childNodes.length;
-                    f++
+                  //layer 5
+                  if (
+                    layer4[d].childNodes[3] &&
+                    layer4[d].childNodes[3].localName == "ol"
                   ) {
-                    if (
-                      sitemap[0].children[b].childNodes[c].children[d]
-                        .childNodes[e].childNodes[f].localName
-                    ) {
-                      let lvl3 =
-                        sitemap[0].children[b].childNodes[c].children[d]
-                          .childNodes[e].childNodes[f].childNodes[1]
-                          .innerText;
-                      three++;
+                    let layer5 = layer4[d].childNodes[3].children;
+
+                    for (let e = 0; e < layer5.length; e++) {
+                      six = 0;
+                      five++;
                       console.log(
-                        `        ${one}.${two}.${three}.${four}.${five}.${six}.${seven}.${eight}.${nine} ${lvl3}`
+                        `      ${one}.${two}.${three}.${four}.${five}.${six}.${seven}.${eight}.${nine} ${
+                          layer5[e].children[0].innerText
+                        }`
                       );
-                      /* Now the fourth*/
-                      for (
-                        let g = 0;
-                        g <
-                        sitemap[0].children[b].childNodes[c].children[d]
-                          .childNodes[e].childNodes[f].childNodes.length;
-                        g++
+                      // Layer six
+                      if (
+                        layer5[e].childNodes[3] &&
+                        layer5[e].childNodes[3].localName == "ol"
                       ) {
-                        four = 0;
-                        if (
-                          sitemap[0].children[b].childNodes[c].children[d]
-                            .childNodes[e].childNodes[f].childNodes[g]
-                            .localName == "ol"
-                        ) {
-                          for (
-                            let h = 0;
-                            h <
-                            sitemap[0].children[b].childNodes[c].children[d]
-                              .childNodes[e].childNodes[f].childNodes[g]
-                              .childNodes.length;
-                            h++
+                        let layer6 = layer5[e].childNodes[3].children;
+
+                        for (let f = 0; f < layer6.length; f++) {
+                          seven = 0;
+                          six++;
+                          console.log(
+                            `        ${one}.${two}.${three}.${four}.${five}.${six}.${seven}.${eight}.${nine} ${
+                              layer6[f].children[0].innerText
+                            }`
+                          );
+                          // Layer seven
+                          if (
+                            layer6[f].childNodes[3] &&
+                            layer6[f].childNodes[3].localName == "ol"
                           ) {
-                            if (
-                              sitemap[0].children[b].childNodes[c].children[d]
-                                .childNodes[e].childNodes[f].childNodes[g]
-                                .childNodes[h].localName == "li"
-                            ) {
-                              four++;
+                            let layer7 = layer6[f].childNodes[3].children;
+
+                            for (let g = 0; g < layer7.length; g++) {
+                              eight = 0;
+                              seven++;
                               console.log(
-                                `            ${one}.${two}.${three}.${four}.${five}.${six}.${seven}.${eight}.${nine} ${
-                                  sitemap[0].children[b].childNodes[c]
-                                    .children[d].childNodes[e].childNodes[f]
-                                    .childNodes[g].childNodes[h].childNodes[1]
-                                    .innerText
+                                `          ${one}.${two}.${three}.${four}.${five}.${six}.${seven}.${eight}.${nine} ${
+                                  layer7[g].children[0].innerText
                                 }`
                               );
-                            }
-                            /* Now the fifth */
-                            five = 0;
-                            for (let i = 0; i <  sitemap[0].children[b].childNodes[c]
-                              .children[d].childNodes[e].childNodes[f]
-                              .childNodes[g].childNodes[h].childNodes.length; i++) {
-                                if (sitemap[0].children[b].childNodes[c]
-                                  .children[d].childNodes[e].childNodes[f]
-                                  .childNodes[g].childNodes[h].childNodes[i].localName == "ol") {
-                                        five++;
-                                        console.log(`                ${one}.${two}.${three}.${four}.${five}.${six}.${seven}.${eight} ${sitemap[0].children[b].childNodes[c]
-                                          .children[d].childNodes[e].childNodes[f]
-                                          .childNodes[g].childNodes[h].childNodes[i].childNodes[1].innerText}`)
-                                  }
-                              }
-                          }
 
+                              // Layer eight
+                              if (
+                                layer7[g].childNodes[3] &&
+                                layer7[g].childNodes[3].localName == "ol"
+                              ) {
+                                let layer8 = layer7[g].childNodes[3].children;
+                                for (let h = 0; h < layer8.length; h++) {
+                                  nine = 0;
+                                  eight++;
+                                  console.log(
+                                    `            ${one}.${two}.${three}.${four}.${five}.${six}.${seven}.${eight}.${nine} ${
+                                      layer8[h].children[0].innerText
+                                    }`
+                                  );
+
+                                  // Layer nine
+                                  if (
+                                    layer8[h].childNodes[3] &&
+                                    layer8[h].childNodes[3].localName == "ol"
+                                  ) {
+                                    let layer9 =
+                                      layer8[h].childNodes[3].children;
+                                    for (let i = 0; i < layer9.length; i++) {
+                                      nine++;
+                                      console.log(
+                                        `            ${one}.${two}.${three}.${four}.${five}.${six}.${seven}.${eight}.${nine} ${
+                                          layer9[i].children[0].innerText
+                                        }`
+                                      );
+                                    }
+                                  }
+                                }
+                              }
+                            }
+                          }
                         }
                       }
                     }
@@ -196,10 +164,7 @@ traverse = () => {
         }
       }
     }
-   }
-      
-    }
   }
-};
+}
 
-traverse();
+mapSite();
